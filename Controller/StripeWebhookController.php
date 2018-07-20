a04381ce460f692127d5cd059d3ea0bce77a35d1
Chris Tanner <chris@annex-apps.com>
2018-07-20 09:49:23 +0100

    Deal with more webhooks

--- Controller/StripeWebhookController.php
+++ Controller/StripeWebhookController.php
@@ -37,6 +37,18 @@ class StripeWebhookController extends Controller
 
         $status = 'Received OK';
         switch ($event_type) {
+            case "customer.subscription.deleted":
+                if ($tenant = $tenantService->getTenantByStripeCustomerId($event->customer)) {
+                    if ($subscription = $tenant->getSubscription()) {
+                        $subscriptionId = $subscription->getId();
+                        if ($tenantService->cancelSubscription($subscriptionId)) {
+                            $this->sendCancelEmail($tenant);
+                        }
+                    }
+                } else {
+                    $status = 'Failed to find tenant using '.$event->customer;
+                }
+                break;
             case "customer.subscription.created":
                 /** @var \Annex\TenantBundle\Entity\Tenant $tenant */
                 if (!$tenant = $tenantService->getTenantByStripeCustomerId($event->customer)) {
@@ -70,6 +82,13 @@ class StripeWebhookController extends Controller
 
                 } else {
                     $status = 'Failed to find tenant for invoice using '.$event->customer;
+                }
+                break;
+            case "invoice.payment_failed":
+                if ($tenant = $tenantService->getTenantByStripeCustomerId($event->customer)) {
+                    $this->sendFailureEmail($tenant);
+                } else {
+                    $status = 'Failed to find tenant for email using '.$event->customer;
                 }
                 break;
         }
@@ -128,8 +147,6 @@ class StripeWebhookController extends Controller
 
         } else {
 
-//            $this->addFlash('error', "Could not find an invoice with ID {$stripeInvoiceId}");
-//            return $this->redirectToRoute('account_billing');
 //            $e = '';
 //            foreach ($stripeService->errors AS $error) {
 //                $e .= $error;
@@ -180,5 +197,86 @@ class StripeWebhookController extends Controller
             'tenant' => $tenant
         ]);
 
+    }
+
+    /**
+     * @param Tenant $tenant
+     */
+    private function sendFailureEmail(Tenant $tenant)
+    {
+        $appName = ucfirst($this->getParameter('app_info.name'));
+        $appUrl  = $this->getParameter('app_info.domain');
+
+        $messageBody = <<<EOB
+We've just tried to charge your credit card for your Annex Apps {$appName} account, but it was rejected by the bank.
+We'll try again in a couple of days. If you'd like to change the card we have on record, please log in to https://{$appUrl}.
+<br><br>
+After three failures, your subscription will be cancelled.
+EOB;
+
+        try {
+            $client = new PostmarkClient($this->getParameter('annex.postmark_api_key'));
+            $message = $this->renderView(
+                'AnnexTenantBundle::emails/basic.html.twig',
+                [
+                    'title' => '',
+                    'heading' => "A charge has failed",
+                    'message' => $messageBody
+                ]
+            );
+
+            $client->sendEmail(
+                "Annex Apps <system@annex-apps.com>",
+                $tenant->getOwnerEmail(),
+                "Credit card fail for ".$tenant->getName(),
+                $message,
+                NULL, NULL, true, NULL, NULL, 'hello@annex-apps.com'
+            );
+
+        } catch (\Exception $generalException) {
+            die("error: ".$generalException->getMessage());
+        }
+    }
+
+    /**
+     * @param Tenant $tenant
+     */
+    private function sendCancelEmail(Tenant $tenant)
+    {
+        $appName = ucfirst($this->getParameter('app_info.name'));
+        $appUrl  = $this->getParameter('app_info.domain');
+
+        $messageBody = <<<EOB
+Your subscription to the {$appName} app has been cancelled.
+This is usually because your card was rejected by the bank over three attempts.
+<br><br>
+If you'd like to re-subscribe, please log in to https://{$appUrl}.
+<br><br>
+We'd love to hear more from you, so that we can improve our service.
+If you can spare a moment for some feedback, please give us a shout at hello@annex-apps.com
+EOB;
+
+        try {
+            $client = new PostmarkClient($this->getParameter('annex.postmark_api_key'));
+            $message = $this->renderView(
+                'AnnexTenantBundle::emails/basic.html.twig',
+                [
+                    'title' => '',
+                    'heading' => "Sorry to see you go.",
+                    'message' => $messageBody
+                ]
+            );
+
+            $client->sendEmail(
+                "Annex Apps <system@annex-apps.com>",
+                $tenant->getOwnerEmail(),
+                "Subscription has been cancelled for ".$tenant->getName(),
+                $message,
+                NULL, NULL, true, NULL, NULL, 'hello@annex-apps.com'
+            );
+
+        } catch (\Exception $generalException) {
+            die("error: ".$generalException->getMessage());
+        }
     }
 }
